2854ec2b84e0df8947aa82fca378bcc044b8f290
Hamza <itxhaxnan@gmail.com>
2024-01-11 02:55:01 +0500

    Navbar

--- components/Navbar/Navbar.jsx
+++ components/Navbar/Navbar.jsx
@@ -1,38 +1,41 @@
-
-import { Box, Flex, Spacer,Text, IconButton, useColorMode} from "@chakra-ui/react";
+import { useState } from "react";
+import { Box, Flex, Text, IconButton, useColorMode } from "@chakra-ui/react";
 import Link from "next/link";
-import { FaMoon} from "react-icons/fa";
+import { FaMoon } from "react-icons/fa";
 import { GiHamburgerMenu } from "react-icons/gi";
 import { IoMdSunny } from "react-icons/io";
 
 const Navbar = () => {
   const { colorMode, toggleColorMode } = useColorMode();
+  const [isMenuOpen, setMenuOpen] = useState(false);
+
+  const handleMenuToggle = () => {
+    setMenuOpen(!isMenuOpen);
+  };
 
   return (
-    <Box bg={colorMode === "light" ? "white" : "black"} 
-        color={colorMode === "light" ? "black" : "white"}  
-        p={4} boxShadow="md">
-      <Flex justifyContent={"space-between"} >
+    <Box
+      bg={colorMode === "light" ? "white" : "black"}
+      color={colorMode === "light" ? "black" : "white"}
+      p={4}
+      boxShadow="md"
+    >
+      <Flex justifyContent={"space-between"}>
         <Link href="/">
-            <Box p="2">
-              <Text>My Ecommerce</Text>
-            </Box>
+          <Box p="2">
+            <Text>My Ecommerce</Text>
+          </Box>
         </Link>
 
-        <Spacer />
-
-        <Flex>
-          
+        <Flex display={{ base: "none", md: "flex" }}>
           <Link href="/products">
-              <Box p="2">Products</Box>
+            <Box p="2">Products</Box>
           </Link>
           <Link href="/cart">
-              <Box p="2">Cart</Box>
+            <Box p="2">Cart</Box>
           </Link>
           {/* Add more links as needed */}
         </Flex>
-
-        <Spacer />
 
         <IconButton
           aria-label="Toggle dark mode"
@@ -42,10 +45,24 @@ const Navbar = () => {
 
         <IconButton
           aria-label="Toggle menu"
-          icon={<GiHamburgerMenu/>}
+          icon={<GiHamburgerMenu />}
           display={{ base: "block", md: "none" }}
+          onClick={handleMenuToggle}
         />
       </Flex>
+
+      {/* Mobile Menu */}
+      {isMenuOpen && (
+        <Flex direction="column" mt={4}>
+          <Link href="/products">
+            <Box p="2">Products</Box>
+          </Link>
+          <Link href="/cart">
+            <Box p="2">Cart</Box>
+          </Link>
+          {/* Add more links as needed */}
+        </Flex>
+      )}
     </Box>
   );
 };
